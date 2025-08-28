5dbdaae0cbc224d34b4ec4fcec80bcd552f9d7d6
hamidrezasoltanian <hamidreza.soltanian@gmail.com>
2025-08-28 12:46:22 +0330

    feat: Initialize React app and render App component
    Sets up the entry point for the React application by importing necessary modules, locating the root DOM element, and rendering the main `App` component. Includes error handling for a missing root element.

---
+++ main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx'; // وارد کردن کامپوننت اصلی
+
+// پیدا کردن تگ root در HTML
+const container = document.getElementById('root');
+
+// اطمینان از وجود تگ root
+if (container) {
+  // ساختن ریشه رندر برای اپلیکیشن
+  const root = createRoot(container);
+  
+  // رندر کردن کامپوننت اصلی در تگ root
+  root.render(<App />);
+} else {
+  console.error('Fatal Error: Root element with id "root" not found in the DOM.');
+}
